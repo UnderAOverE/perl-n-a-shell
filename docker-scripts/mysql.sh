9a13a69c8eb360c4e7f9a6da1e06f37577ceeffb
R2D2-C3P0 <r2d2c3p0@users.noreply.github.com>
2017-12-12 14:38:42 -0600

    Add files via upload
    Shell scripts with docker run commands for popular web/db tools.

---
+++ docker-scripts/mysql.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+#
+# 12/10/2017.
+# r2d2c3p0.
+# mysql.sh
+# MySQL docker run command.
+# v1.0.0
+#
+
+# command arguments. want more? use: docker run --help
+# --name = name of the container.
+# --volume = maps local filesystem.
+# --publish = maps the container port to the host.
+# --detach = run in background.
+# --env = environment property, MYSQL_ALLOW_EMPTY_PASSWORD=yes, sets no password.
+
+docker run \
+	--name shane-mysql \
+	--env MYSQL_ALLOW_EMPTY_PASSWORD=yes \
+	--volume /home/shane/dockerdata/mySQL:/var/lib/mysql \
+	--publish 3306:3306 \
+	--detach mysql
+
+#end_mysql.sh

6a7159bb8dda11d09651e1c2024e02aa38dc2bc7
R2D2-C3P0 <r2d2c3p0@users.noreply.github.com>
2018-02-26 10:54:50 -0600

    Update certificate-hash-check.bash

--- openssl/certificate-hash-check.bash
+++ openssl/certificate-hash-check.bash
@@ -1,3 +1,5 @@
+# Script to check if CA is Symantec, DigiCert replaced Symantec.
+# 2/26/2018, may not be useful in 2019!!!
 
 # https://cs.chromium.org/chromium/src/net/data/ssl/symantec/README.md
 # https://cs.chromium.org/chromium/src/net/cert/symantec_certs.cc
@@ -6,4 +8,6 @@ for i in certs.pem; do
 	openssl x509 -noout -pubkey -in "${i}" | openssl asn1parse -inform pem -out public.key -noout
 	digest=`cat public.key | openssl dgst -sha256 -c | awk -F " " '{print $2}' | sed s/:/,0x/g`
 	echo "0x${digest} ${i##*/}";
-done 
+done
+
+#end.

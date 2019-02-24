531ed420429c1bd91718291870604647f4db3d67
R2D2-C3P0 <r2d2c3p0@users.noreply.github.com>
2019-02-23 22:26:21 -0600

    Create 3_minutes.sh

---
+++ 3_minutes.sh
@@ -0,0 +1,4 @@
+minutes_3=`date --date="-3 min" "+%a %h %d %H:%M"`
+minutes_2=`date --date="-2 min" "+%a %h %d %H:%M"`
+minutes_1=`date --date="-1 min" "+%a %h %d %H:%M"`
+minutes_0=`date --date="-0 min" "+%a %h %d %H:%M"`

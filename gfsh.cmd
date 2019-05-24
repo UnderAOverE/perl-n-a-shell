a0d897338ce0f90340a6ad481306237342ff1a3b
Shane Reddy <r2d2c3p0@users.noreply.github.com>
2019-05-23 23:55:07 -0500

    Update gfsh.cmd

--- gfsh.cmd
+++ gfsh.cmd
@@ -1,1 +1,3 @@
-java.exe -Dgfsh=true -classpath "pivotal-gemfire-9.3.0\lib\gfsh-dependencies.jar";"pivotal-gemfire-9.3.0\lib" org.apache.geode.management.internal.cli.Launcher
+java.exe -Dgfsh=true \
+  -classpath "pivotal-gemfire-9.3.0\lib\gfsh-dependencies.jar"; \
+  "pivotal-gemfire-9.3.0\lib" org.apache.geode.management.internal.cli.Launcher

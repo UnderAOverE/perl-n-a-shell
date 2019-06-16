f0179b6f0955791a5aa09a4d2ef540d7ae101e11
Shane Reddy <r2d2c3p0@users.noreply.github.com>
2019-06-16 02:59:35 -0500

    Update tls_ssl_ciphers_check.sh

--- openssl/tls_ssl_ciphers_check.sh
+++ openssl/tls_ssl_ciphers_check.sh
@@ -44,6 +44,10 @@ print_usage() {
 	exit ${ERR_USAGE}
 }
 
+Echo() {
+	echo "${@}" | tee -a tls_ssl_ciphers_check.log
+}
+
 # main.
 if [[ $(kill -l | grep -c SIG) -eq 0 ]]; then
 	printf "\n\033[1;35m Please make sure you're calling me without leading \"sh\"! Bye...\033[m\n\n" >&2 && exit ${ERR_BASH}
@@ -108,9 +112,11 @@ for connection_string in $(cat intranet.txt); do
 	connection_host=$(echo ${connection_string} | awk -F "," '{print $2}')
 	connection_port=$(echo ${connection_string} | awk -F "," '{print $3}')
 	echo "-----------------------------------------------------------------"
+	Echo "-----------------------------------------------------------------"
 	nslookup ${connection_host} >/dev/null 2>&1
 	if [[ $? -eq 0 ]]; then
 		echo " ${connection_name}=${connection_host}:${connection_port}"
+		Echo " ${connection_name}=${connection_host}:${connection_port}"
 		timeout 1 bash -c "</dev/tcp/${connection_host}/${connection_port}" >/dev/null 2>&1
 		if [[ $? -eq 0 ]]; then
 			#for ciphers_a in $(openssl ciphers 'ALL:eNULL' | tr ':' ' '); do
@@ -121,13 +127,15 @@ for connection_string in $(cat intranet.txt); do
 					echo | openssl s_client -connect ${connection_host}:${connection_port} -tls1_2 -cipher "${Cipher}" -msg 2>/dev/null | grep ChangeCipherSpec >/dev/null 2>&1
 					if [[ $? -eq 0 ]]; then
 						echo "  ${connection_host}[:${connection_port}] using [TLSv1.2] with ${Cipher} --> ${BLINK}${GREEN}Passed${NORMAL}."
+						Echo "  ${connection_host}[:${connection_port}] using [TLSv1.2] with ${Cipher} --> Passed."
 						tls12_check=1
 					else
 						x=0;
 						#echo "  ${connection_host}[:${connection_port}] using [TLSv1.2] with ${Cipher} --> ${BLINK}${RED}Failed${NORMAL}."
+						#Echo "  ${connection_host}[:${connection_port}] using [TLSv1.2] with ${Cipher} --> Failed."
 					fi
 				done
-				[[ ${tls12_check} -eq 0 ]] && { echo "  ${connection_host}[:${connection_port}] using [TLSv1.2] --> ${BLINK}${YELLOW}Not Allowed${NORMAL}." ; }
+				[[ ${tls12_check} -eq 0 ]] && { echo "  ${connection_host}[:${connection_port}] using [TLSv1.2] --> ${BLINK}${YELLOW}Not Allowed${NORMAL}." ; Echo "  ${connection_host}[:${connection_port}] using [TLSv1.2] --> Not Allowed." ; }
 			fi
 			if [[ ${protocol_tls11} -eq 1 ]]; then
 				tls11_check=0
@@ -135,13 +143,15 @@ for connection_string in $(cat intranet.txt); do
 					echo | openssl s_client -connect ${connection_host}:${connection_port} -tls1_1 -cipher "${Cipher}" -msg 2>/dev/null | grep ChangeCipherSpec >/dev/null 2>&1
 					if [[ $? -eq 0 ]]; then
 						echo "  ${connection_host}[:${connection_port}] using [TLSv1.1] with ${Cipher} --> ${BLINK}${GREEN}Passed${NORMAL}."
+						Echo "  ${connection_host}[:${connection_port}] using [TLSv1.1] with ${Cipher} --> Passed."
 						tls11_check=1
 					else
 						x=0;
 						#echo "  ${connection_host}[:${connection_port}] using [TLSv1.1] with ${Cipher} --> ${BLINK}${RED}Failed${NORMAL}."
+						#Echo "  ${connection_host}[:${connection_port}] using [TLSv1.1] with ${Cipher} --> Failed."
 					fi
 				done
-				[[ ${tls11_check} -eq 0 ]] && { echo "  ${connection_host}[:${connection_port}] using [TLSv1.1] --> ${BLINK}${YELLOW}Not Allowed${NORMAL}." ; }
+				[[ ${tls11_check} -eq 0 ]] && { echo "  ${connection_host}[:${connection_port}] using [TLSv1.1] --> ${BLINK}${YELLOW}Not Allowed${NORMAL}." ; Echo "  ${connection_host}[:${connection_port}] using [TLSv1.1] --> Not Allowed." ; }
 			fi
 			if [[ ${protocol_tls10} -eq 1 ]]; then
 				tls10_check=0
@@ -149,13 +159,15 @@ for connection_string in $(cat intranet.txt); do
 					echo | openssl s_client -connect ${connection_host}:${connection_port} -tls1 -cipher "${Cipher}" -msg 2>/dev/null | grep ChangeCipherSpec >/dev/null 2>&1
 					if [[ $? -eq 0 ]]; then
 						echo "  ${connection_host}[:${connection_port}] using [TLSv1.0] with ${Cipher} --> ${BLINK}${GREEN}Passed${NORMAL}."
+						Echo "  ${connection_host}[:${connection_port}] using [TLSv1.0] with ${Cipher} --> Passed."
 						tls10_check=1
 					else
 						x=0;
 						#echo "  ${connection_host}[:${connection_port}] using [TLSv1.0] with ${Cipher} --> ${BLINK}${RED}Failed${NORMAL}."
+						#Echo "  ${connection_host}[:${connection_port}] using [TLSv1.0] with ${Cipher} --> Failed."
 					fi
 				done
-				[[ ${tls10_check} -eq 0 ]] && { echo "  ${connection_host}[:${connection_port}] using [TLSv1.0] --> ${BLINK}${YELLOW}Not Allowed${NORMAL}." ; }
+				[[ ${tls10_check} -eq 0 ]] && { echo "  ${connection_host}[:${connection_port}] using [TLSv1.0] --> ${BLINK}${YELLOW}Not Allowed${NORMAL}." ; Echo "  ${connection_host}[:${connection_port}] using [TLSv1.0] --> Not Allowed." ; }
 			fi
 			if [[ ${protocol_ssl3} -eq 1 ]]; then
 				ssl3_check=0
@@ -163,13 +175,15 @@ for connection_string in $(cat intranet.txt); do
 					echo | openssl s_client -connect ${connection_host}:${connection_port} -ssl3 -cipher "${Cipher}" -msg 2>/dev/null | grep ChangeCipherSpec >/dev/null 2>&1
 					if [[ $? -eq 0 ]]; then
 						echo "  ${connection_host}[:${connection_port}] using [SSLv3] with ${Cipher} --> ${BLINK}${GREEN}Passed${NORMAL}."
+						Echo "  ${connection_host}[:${connection_port}] using [SSLv3] with ${Cipher} --> Passed."
 						ssl3_check=1
 					else
 						x=0;
 						#echo "  ${connection_host}[:${connection_port}] using [SSLv3] with ${Cipher} --> ${BLINK}${RED}Failed${NORMAL}."
+						#Echo "  ${connection_host}[:${connection_port}] using [SSLv3] with ${Cipher} --> Failed."
 					fi
 				done
-				[[ ${ssl3_check} -eq 0 ]] && { echo "  ${connection_host}[:${connection_port}] using [SSLv3] --> ${BLINK}${YELLOW}Not Allowed${NORMAL}." ; }
+				[[ ${ssl3_check} -eq 0 ]] && { echo "  ${connection_host}[:${connection_port}] using [SSLv3] --> ${BLINK}${YELLOW}Not Allowed${NORMAL}." ; Echo "  ${connection_host}[:${connection_port}] using [SSLv3] --> Not Allowed." ; }
 			fi
 			if [[ ${protocol_ssl2} -eq 1 ]]; then
 				ssl2_check=0
@@ -177,21 +191,26 @@ for connection_string in $(cat intranet.txt); do
 					echo | openssl s_client -connect ${connection_host}:${connection_port} -ssl2 -cipher "${Cipher}" -msg 2>/dev/null | grep ChangeCipherSpec >/dev/null 2>&1
 					if [[ $? -eq 0 ]]; then
 						echo "  ${connection_host}[:${connection_port}] using [SSLv2] with ${Cipher} --> ${BLINK}${GREEN}Passed${NORMAL}."
+						Echo "  ${connection_host}[:${connection_port}] using [SSLv2] with ${Cipher} --> Passed."
 						ssl2_check=1
 					else
 						x=0;
 						#echo "  ${connection_host}[:${connection_port}] using [SSLv2] with ${Cipher} --> ${BLINK}${RED}Failed${NORMAL}."
+						#Echo "  ${connection_host}[:${connection_port}] using [SSLv2] with ${Cipher} --> Failed."
 					fi
 				done
-				[[ ${ssl2_check} -eq 0 ]] && { echo "  ${connection_host}[:${connection_port}] using [SSLv2] --> ${BLINK}${YELLOW}Not Allowed${NORMAL}." ; }
+				[[ ${ssl2_check} -eq 0 ]] && { echo "  ${connection_host}[:${connection_port}] using [SSLv2] --> ${BLINK}${YELLOW}Not Allowed${NORMAL}." ; Echo "  ${connection_host}[:${connection_port}] using [SSLv2] --> Not Allowed." ; }
 			fi
 		else
-			echo "  ${connection_host}:${connection_port} ${BLINK}${RED}Refused$${NORMAL}."
+			echo "  ${connection_host}:${connection_port} --> ${BLINK}${RED}Connection Refused${NORMAL}."
+			Echo "  ${connection_host}:${connection_port} --> Connection Refused."
 		fi
 	else
-		echo "  ${connection_host} ${BLINK}${RED}NOT found${NORMAL}."
+		echo "  ${connection_host} --> ${BLINK}${RED}NOT found${NORMAL}."
+		echo "  ${connection_host} --> NOT found."
 	fi
 done
 echo "-----------------------------------------------------------------"
+Echo "-----------------------------------------------------------------"
 
 #end_tls_ssl_ciphers_check.sh
